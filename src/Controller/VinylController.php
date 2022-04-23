5d8524b7dbab1f0e250c6b07a86214699a31a11c
MattCornicDev <glgharib@gmail.com>
2022-04-23 23:57:11 +0200

    route, controller, reponse

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,12 +4,29 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class VinylController
 {
     #[Route('/home', name: 'home')]
-     public function homepage():Response
+     public function homepage(): Response
      {
-          return 'lal';
+          return new Response('ok !');
+     }
+
+     #[Route('/browse/{slug}')]
+     public function browser(string $slug = null): Response
+     {
+         if ($slug)
+         {
+             $title = 'Genre : ' . u(str_replace('-', ' ', $slug))->title(true);
+         }else
+         {
+             $title = 'Tous genres';
+         }
+
+
+         return new Response($title);
+
      }
 }
